c1e861b2da09332ee76adef1d92862dfebc22d61
Craig Spreeman <craigspreeman@gmail.com>
2019-06-28 07:40:46 -0700

    Update to rotation and flips
    Update to rotation and flips

--- test.py
+++ test.py
@@ -4,12 +4,14 @@ from datetime import datetime
 from validate import Validate
 import time
 
-start_time = str(datetime.now())
+start_time = time.strftime("%Y%m%d-%H%M%S")
 
 validate = Validate()
-'''
+
 tello = Tello()
-'''
+
+tello.send_command('command')
+
 while True:
     print('Please enter a command...')
     command = raw_input()
@@ -21,19 +23,16 @@ while True:
 
     if valid:
         print('Valid ' + valid_command)
-        '''
         tello.send_command(valid_command)
-        '''
     else:
         print('Invalid Command...')
 
-'''
 log = tello.get_log()
 
-out = open('log/' + start_time + '.txt', 'w')
+out = open('C:/Users/Craig/Desktop/Tello-master/log/' + start_time + '.txt', 'w')
 for stat in log:
     stat.print_stats()
     str = stat.return_stats()
     out.write(str)
-'''
+
 
--- validate.py
+++ validate.py
@@ -3,6 +3,9 @@ from sets import Set
 valid_auto_commands = Set(['takeoff','exit', 'stop'])
 valid_data_commands = Set(['battery?','time?','wifi?', 'speed?'])
 valid_move_commands = Set(['up', 'down', 'left', 'right', 'forward', 'back'])
+valid_rotate_commands = Set(['cw', 'ccw'])
+valid_directions = Set(['l','r','f','b'])
+
 
 class Validate:
 
@@ -36,8 +39,30 @@ class Validate:
                 if 10 <= speed <= 100:
                     command += ' '
                     command += `speed`
+                    valid = True
                 else:
                     print('Invalid value...')
+            elif command in valid_rotate_commands:
+                print('Please enter a rotation in degrees 1-360...')
+                rotate = int(raw_input())
+                if 1 <= rotate <= 360:
+                    command += ' '
+                    command += `rotate`
+                    valid = True
+                else:
+                    print('Invalid value...')
+            elif command == 'flip':
+                print('Please enter a direction to flip (l,r,f,b)....')
+                direction = raw_input()
+                direction = direction.rstrip().lower()
+                if direction in valid_directions:
+                    command += ' '
+                    command += direction
+                    valid = True
+                else:
+                    print('Invalid value...')
+            elif command == 'take picture':
+                valid = True
             else:
                 valid = False
         except:
